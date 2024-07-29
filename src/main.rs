39b14935a0b6a3778a1238919def0b7f1e435b0a
Mueez Abdur-Rahman <mueez.abdurrahman@gmail.com>
2024-07-29 16:20:43 +0100

    Initial commit. Started relaxing sequentially.

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod sequential_relaxer;
+
+fn main() {
+    println!("Starting rusty relaxation.");
+
+    sequential_relaxer::relax();
+}
---
+++ src/sequential_relaxer.rs
@@ -0,0 +1,5 @@
+
+pub fn relax() -> bool {
+    println!("Starting to relax sequentially.");
+    return true;
+}
