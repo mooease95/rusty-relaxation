baa34c85b6cea220681317f8ba8facc32b6ebfe5
Mueez Abdur-Rahman <mueez.abdurrahman@gmail.com>
2024-08-07 11:14:11 +0100

    Solved sequentially for 4x4 array.

--- src/relaxation_utils.rs
+++ src/relaxation_utils.rs
@@ -1,10 +1,12 @@
 
-/*
-    public static double averageArray(double[][] array, int x, int y) {
-        return (array[x][y-1] + array[x][y+1] + array[x-1][y] + array[x+1][y])/4;
-    }
- */
 
-fn average_array(array: [[i32; 4]; 4], x: i32, y: i32) -> i32 {
-    let value = (array[x][y-1] + array[x][y+1] + array[x-1][y] + array[x+1][y])/4; // TODO: <=== What the hell is this issue? Work here.
+pub fn check_precision(correct_array: [[f64; 4]; 4], relaxed_value:f64, row: usize, column: usize, target_precision: f64) -> bool {
+    let correct_value: f64 = correct_array[row][column];
+    let precision_reached: f64 = (correct_value - relaxed_value).abs();
+    return precision_reached <= target_precision;
+}
+
+pub fn average_array(array: [[f64; 4]; 4], x: usize, y: usize) -> f64 {
+    let value: f64 = array[x][y-1] + array[x][y+1] + array[x-1][y] + array[x+1][y];
+    return value / 4.0;
 }
--- src/sequential_relaxer.rs
+++ src/sequential_relaxer.rs
@@ -1,3 +1,6 @@
+
+use crate::relaxation_utils as utils;
+
 pub fn relax() -> bool {
     println!("Starting to relax sequentially.");
 
@@ -13,9 +16,23 @@ pub fn relax() -> bool {
     // 3 |  3   4   5   6
     // =============================================
 
-    let input_array: [[i32; 4]; 4] = [[0, 1, 2, 3], [1, 0, 0, 4], [2, 0, 0, 5], [3, 4, 5, 6]];
+    let correct_array: [[f64; 4]; 4] = [[0.0, 1.0, 2.0, 3.0], [1.0, 2.0, 3.0, 4.0], [2.0, 3.0, 4.0, 5.0], [3.0, 4.0, 5.0, 6.0]];
+    let target_precision: f64 = 0.1;
+
+    println!("Correct array:");
+    for n in 0..correct_array.len() {
+        println!("{:?}", correct_array[n]);
+    }
+
+    println!("Target precision=[{}].", target_precision);
+
+    let mut input_array: [[f64; 4]; 4] = [[0.0, 1.0, 2.0, 3.0], [1.0, 0.0, 0.0, 4.0], [2.0, 0.0, 0.0, 5.0], [3.0, 4.0, 5.0, 6.0]];
     let size: usize = input_array.len();
-    println!("Size of array is={}.", size);
+
+    println!("Input array before relaxing:");
+    for n in 0..size {
+        println!("{:?}", input_array[n]);
+    }
 
     let mut steps_taken: i32 = 0;
 
@@ -24,13 +41,8 @@ pub fn relax() -> bool {
         needs_another_iteration = false;
         steps_taken += 1;
 
-        println!("Input array:");
-        for n in 0..size {
-            println!("{:?}", input_array[n]);
-        }
-
         // Copy to a new array.
-        let mut new_array_to_relax: [[i32; 4]; 4] = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+        let mut new_array_to_relax: [[f64; 4]; 4] = [[0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]];
         for n in 0..size {
             new_array_to_relax[n] = input_array[n];
         }
@@ -40,23 +52,22 @@ pub fn relax() -> bool {
             println!("{:?}", new_array_to_relax[n]); // This needs a :? because it's printing an array.
         }
 
-        for row in 0..size {
-            for column in 0..size {
-                println!("{}", new_array_to_relax[row][column]) // This only prints a number so does not need a :?.
-                // TODO: Do the average here and continue. <=== WORK HERE!!!
-                /*
-                double newAvgValue = RelaxerUtils.averageArray(newArrayToRelax, row, column);
-                arrayToRelax[row][column] = newAvgValue;
-                boolean precisionReachedForCurrentValue = RelaxerUtils.checkPrecision(relaxableArray, newAvgValue, row, column, targetPrecision);
-                if (!precisionReachedForCurrentValue) {
-                   needsAnotherIteration = true; // if we haven't reached precision, we need another iteration
+        for row in 1..size-1 {
+            for column in 1..size-1 {
+                let new_avg_value = utils::average_array(new_array_to_relax, row, column);
+                input_array[row][column] = new_avg_value;
+                let precision_reached_for_current_value: bool = utils::check_precision(correct_array, new_avg_value, row, column, target_precision);
+                if !precision_reached_for_current_value {
+                    needs_another_iteration = true;
                 }
-                 */
-
-                let mut new_avg_value =
             }
         }
     }
+    println!("PRECISION REACHED FOR ALL!! Steps taken=[{}].", steps_taken);
 
+    println!("Input array after relaxing:");
+    for n in 0..size {
+        println!("{:?}", input_array[n]);
+    }
     return true;
 }
